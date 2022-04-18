2f4c6a6c8d4720963b57f9ee9cb90690adbae969
Kimiro34 <ermoshinvladislav34@gmail.com>
2022-04-18 22:09:02 +0100

    agent storage added

--- elixir/01/kv/test/kv_test.exs
+++ elixir/01/kv/test/kv_test.exs
@@ -1,8 +1,15 @@
-defmodule KVTest do
-  use ExUnit.Case
-  doctest KV
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert KV.hello() == :world
+  setup do
+    {:ok, bucket} = KV.Bucket.start_link([])
+    %{bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
   end
 end
