5153215b9b2c20f58ae9885e9052e8f57f45b7f8
Kimiro34 <ermoshinvladislav34@gmail.com>
2022-04-19 07:10:10 +0100

    add supervisor

--- elixir/01/kv/test/kv_test.exs
+++ elixir/01/kv/test/kv_test.exs
@@ -12,4 +12,17 @@ defmodule KV.BucketTest do
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
+
+  test "removes bucket on crash", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    # Stop the bucket with non-normal reason
+    Agent.stop(bucket, :shutdown)
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+
+  test "are temporary workers" do
+    assert Supervisor.child_spec(KV.Bucket, []).restart == :temporary
+  end
 end
